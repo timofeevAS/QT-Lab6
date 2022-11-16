9eff77e39b4dbe04046c1caed3d345559225322e
unknown <timofeev2.as@edu.spbstu.ru>
2022-11-16 20:23:03 +0300

    vers2

--- main.cpp
+++ main.cpp
@@ -16,31 +16,6 @@ int main(int argc, char *argv[])
     //QLabel *label  = new QLabel();
 
     MyWidgets my_wdgts;
-
-    //QPushButton *btn1 = new QPushButton("Create Spinbox");
-    //QPushButton *btn2 = new QPushButton("Create Scrollbar");
-    //QPushButton *btn4 = new QPushButton("Create Slider");
-    //QPushButton *btn5 = new QPushButton("Create Label");
-    //QPushButton *btn3 = new QPushButton("Connect");
-
-
-    //QVBoxLayout *vlay = new QVBoxLayout();
-    //QHBoxLayout *hlay = new QHBoxLayout(label);
-    //vlay->addWidget(btn1);
-    //vlay->addWidget(btn2);
-    //|vlay->addWidget(btn4);
-    //vlay->addWidget(btn5);
-   //vlay->addWidget(btn3);
-    //hlay->addLayout(vlay);
-    //MyWidgets::connect(btn1,SIGNAL(clicked()),&my_wdgts,SLOT(push_newSpinbox()));
-    //MyWidgets::connect(btn2,SIGNAL(clicked()),&my_wdgts,SLOT(push_newScrollbar()));
-    //MyWidgets::connect(btn4,SIGNAL(clicked()),&my_wdgts,SLOT(push_newSlider()));
-    //MyWidgets::connect(btn5,SIGNAL(clicked()),&my_wdgts,SLOT(push_newLabel()));
-    //MyWidgets::connect(btn3,SIGNAL(clicked()),&my_wdgts,SLOT(connect_Widgets()));
-
-
-    //label->show();
-
     my_wdgts.show();
 
     return a.exec();
